d3e7a5fab54a74e11f72fc1fc2b9a970273321e3
Khushi Yadav <khushiyadav1254@gmail.com>
2026-06-01 11:21:28 +0530

    Add temperature variable and type checking examples

--- Chapter_2.py datatype.py
+++ Chapter_2.py datatype.py
@@ -4,10 +4,16 @@ age=20
 
 area=34.6
 
+temperature=-15.5
+
 print(type(name))
 #output: string data type ayega
+
+print(type(age))
+#output: int data type hoga
 
 print(type(area))
 #output: float datatype hoga
 
-
+print(type(temperature))
+#output: float data type hoga (negative float)
