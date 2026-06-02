c32172cee98e2f8ba6559a5c1cad36ba7acda833
Khushi Yadav <khushiyadav1254@gmail.com>
2026-06-02 14:58:06 +0530

    Add: Print table of 5 using REPL with print statements

--- Problem2.py
+++ Problem2.py
@@ -1,12 +1,13 @@
 # use REPL and print the table of 5 using it.
 
-5=5*1
-5=5*2
-5=5*3
-5=5*4
-5=5*6
-5=5*7
-5=5*8
-5=5*9
-5=5*10
-
+# REPL का use करके 5 की table print करना
+print(5 * 1)
+print(5 * 2)
+print(5 * 3)
+print(5 * 4)
+print(5 * 5)
+print(5 * 6)
+print(5 * 7)
+print(5 * 8)
+print(5 * 9)
+print(5 * 10)
