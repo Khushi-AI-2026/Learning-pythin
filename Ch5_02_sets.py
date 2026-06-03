cab062c6e30489120575f68c928bd9736b3097ba
Khushi Yadav <khushiyadav1254@gmail.com>
2026-06-03 23:29:21 +0530

    Add initial set examples in Ch5_02_sets.py
    Initialize sets and demonstrate usage in Ch5_02_sets.py

---
+++ Ch5_02_sets.py
@@ -0,0 +1,27 @@
+d ={} # empty dictionary 
+s ={} # empty sets
+s = {1,5,32,54,5,5,5}
+
+empty
+e = set{} # dont use s={} as it will create an empty dictionary 
+print(s)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
