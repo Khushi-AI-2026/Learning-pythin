c889546f298e53d240f30545f9332b0049ce0133
Khushi Yadav <khushiyadav1254@gmail.com>
2026-06-03 15:03:05 +0530

    Create Ch4_01_list.py

---
+++ Ch4_01_list.py
@@ -0,0 +1,9 @@
+friends = ["Apple"," orange",5,345.06,false,"akash","rohan"]
+
+print(friends[0])#apple ayega output
+friends[0] = "graphes" #unlike strings lists are mutable
+
+print(friends[6])#rohan ayega output
+
+print(friends[1:4])# output:  "orange",5,345.06
+
